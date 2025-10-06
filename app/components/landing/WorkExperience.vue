5d8dd56946c67e53155723fdd054a1f321c91225
Vitor <vhcassol@gmail.com>
2025-10-06 17:46:57 -0400

    fix: Arrumar Alguns Bugs de Building

--- app/components/landing/WorkExperience.vue
+++ app/components/landing/WorkExperience.vue
@@ -43,7 +43,10 @@ defineProps<{
               :style="{ color: experience.company.color }"
             >
               <span class="font-medium">{{ experience.company.name }}</span>
-              <UIcon :name="experience.company.logo" />
+              <UIcon 
+                v-if="experience.company.logo"
+                :name="experience.company.logo" 
+              />
             </div>
           </ULink>
         </Motion>
